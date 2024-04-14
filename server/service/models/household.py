dc54d82bf36b5a62ab0b602ae30ba156a09fbd59
Pooja-2709 <Pooja-2709>
2024-04-14 14:43:10 +0530

    add escalation models in user models

--- server/service/models/household.py
+++ server/service/models/household.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
+houseitembase = declarative_base()
+class HouseItem(houseitembase):
+    __tablename__ = 'houseitem'
+    __table_args__ = {'schema': 'meta'} 
 
+    item_id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    min_watt_range = Column(Integer)
+    max_watt_range = Column(Integer)
+    
+    
 class HouseItemSchema(BaseModel):
     item_id: int
     item_name: str
--- server/service/models/user.py
+++ server/service/models/user.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, ARRAY, DateTime, func
+from sqlalchemy import Column, Integer, String, ARRAY, DateTime, func, ForeignKey
 import uuid
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 from pydantic import BaseModel
 from typing import List
+from sqlalchemy.orm import relationship
 
-UserBase = declarative_base()
+Base = declarative_base()
 
-class User(UserBase):
+class User(Base):
     __tablename__ = "users"
     __table_args__ = {'schema': 'meta'} 
 
@@ -30,3 +31,16 @@ class UserData(BaseModel):
     ConsumptionReport: List[str]
     HouseholdItems: List[str]
     SubmeterCount: int
+
+class Escalation(Base):
+    __tablename__ = 'escalation'
+    __table_args__ = {'schema': 'meta'} 
+    
+    id = Column(UUID(as_uuid=True), default=uuid.uuid1, primary_key=True, unique=True, index=True, nullable=False)
+    escalation_name = Column(String, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('meta.users.id'))
+    user = relationship(User)  # Establish the relationship with the User table
+    mobile = Column(ARRAY(Integer), unique=True)
+    email = Column(ARRAY(String), unique=True)
+    created_at = Column(Integer, server_default=func.extract('epoch', func.now()))
+    updated_at = Column(Integer, onupdate=func.extract('epoch', func.now()), server_default='0')
